726452f6eaf21ee55437741983ee41f6a26212a1
Yuekio <jlarios_91@hotmail.com>
2024-06-01 01:38:03 -0500

    Entrega de todos los ejercicios

--- CodeTest/Controllers/HomeController.cs
+++ CodeTest/Controllers/HomeController.cs
@@ -3,6 +3,11 @@ using System.Linq;
 using System.Web.Mvc;
 using Quote.Contracts;
 using Quote.Models;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Net;
+using Newtonsoft.Json;
+using System.Collections.Generic;
 
 namespace PruebaIngreso.Controllers
 {
@@ -38,24 +43,89 @@ namespace PruebaIngreso.Controllers
                 Language = Language.Spanish
             };
 
-            var result = this.quote.Quote(request);
-            var tour = result.Tours.FirstOrDefault();
-            ViewBag.Message = "Test 1 Correcto";
-            return View(tour);
+            try
+            {
+                var result = this.quote.Quote(request);
+                var tour = result.Tours.FirstOrDefault() ?? new Tour();
+                ViewBag.Message = "Test 1 Correcto";
+                if (tour != null)
+                    return View(tour);
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+
+            return View(new Tour());
         }
 
+        [Route("Home/Test2")]
         public ActionResult Test2()
         {
             ViewBag.Message = "Test 2 Correcto";
             return View();
         }
 
-        public ActionResult Test3()
+        public class MarginResponse
         {
-            return View();
+            public decimal Margin { get; set; }
         }
 
-        public ActionResult Test4()
+        public static async Task<string> GetMargin(string code)
+        {
+            ServicePointManager.SecurityProtocol = SecurityProtocolType.Tls12;
+            HttpClient client = new HttpClient();
+            string url = String.Format("https://refactored-pancake.free.beeceptor.com/margin/{0}", code);
+            string margin = "{ \"margin\": 0.0 }";
+
+            try
+            {
+                var response = await client.GetAsync(url);
+                response.EnsureSuccessStatusCode();
+
+                if (response.StatusCode == HttpStatusCode.OK)
+                    margin = await response.Content.ReadAsStringAsync();
+            }
+            catch (HttpRequestException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+
+            return margin;
+        }
+
+        public async Task<ActionResult> Test3()
+        {
+            string code = Uri.EscapeDataString("E-U10-DSCVCOVE 404");
+            var json = await GetMargin(code);
+            return Content(json, "application/json");
+        }
+
+        public class TourQuoteDecorator : IMarginProvider
+        {
+            public readonly TourQuote _tourQuote;
+
+            public TourQuoteDecorator(TourQuote tourQuote)
+            {
+                _tourQuote = tourQuote;
+            }
+            public decimal margin => GetMargin(_tourQuote.ContractService.ServiceCode);
+
+            public decimal GetMargin(string code)
+            {
+                var marginJson = "";
+                Task.Run(async () =>
+                {
+                    marginJson = await HomeController.GetMargin(code);
+                }).GetAwaiter().GetResult();
+
+                var marginData = JsonConvert.DeserializeObject<MarginResponse>(marginJson);
+                return marginData?.Margin ?? 0.0m;
+            }
+        }
+
+        public async Task<ActionResult> Test4()
         {
             var request = new TourQuoteRequest
             {
@@ -73,7 +143,15 @@ namespace PruebaIngreso.Controllers
             };
 
             var result = this.quote.Quote(request);
-            return View(result.TourQuotes);
+            var decoratedQuotes = new List<TourQuoteDecorator>();
+
+            foreach (var quote in result.TourQuotes)
+            {
+                var decorator = new TourQuoteDecorator(quote);
+                decoratedQuotes.Add(decorator);
+            }
+
+            return View(decoratedQuotes);
         }
     }
 }
